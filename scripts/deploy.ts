940a18d585dbd7922cb0c3492d9acf7b9810ecfc
Gauthier <legrangm@gmail.com>
2022-10-17 17:21:43 +0200

    update deploy: wip

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,13 +3,15 @@ import { ethers } from "hardhat";
 
 async function main() {
 
+	const [deployer] = await ethers.getSigners();
+
 	const oracle = '0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419';
 	const usdc = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
 	const weth = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 
 
 	const Winible = await ethers.getContractFactory("Winible");
-	const winible = await Winible.deploy(oracle, usdc, weth);
+	const winible = await Winible.deploy(oracle, usdc, weth, deployer.address);
 
 	await winible.deployed();
 
